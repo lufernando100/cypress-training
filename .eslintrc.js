2b02784364380e772ccc79c18f88e865dc3420fb
Luis Fernando Hernandez Fernandez <luis.hernandezf@perficient.com>
2022-08-23 10:46:26 -0500

    Update package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,33 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: 'standard-with-typescript',
-  overrides: [
-  ],
+  extends: "standard-with-typescript",
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    "quotes": ["error", "double"],
+    quotes: ["error", "double"],
     "cypress/no-assigning-return-values": "error",
     "cypress/no-unnecessary-waiting": "error",
     "cypress/assertion-before-screenshot": "warn",
     "cypress/no-force": "warn",
     "no-unused-vars": "warn",
     "require-jsdoc": "warn",
-    "max-len": [ "error", { "code": 120 } ]
+    "max-len": ["error", { code: 120 }],
   },
-  "plugins": [
-    "@typescript-eslint",
-    "cypress"
-  ]  
-}
+  plugins: ["@typescript-eslint", "cypress"],
+  rules: {
+    quotes: ["error", "double"],
+    "cypress/no-assigning-return-values": "error",
+    "cypress/no-unnecessary-waiting": "error",
+    "cypress/assertion-before-screenshot": "warn",
+    "cypress/no-force": "warn",
+    "no-unused-vars": "warn",
+    "require-jsdoc": "warn",
+    "max-len": ["error", { code: 120 }],
+  },
+};
